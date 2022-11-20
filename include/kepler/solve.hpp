8c5b73d6a1111d29d309c15e3f34734b6127f17b
Dan F-M <foreman.mackey@gmail.com>
2022-11-19 23:26:17 -0500

    range reduction

--- include/kepler/solve.hpp
+++ include/kepler/solve.hpp
@@ -1,8 +1,11 @@
 #ifndef KEPLER_SOLVE_HPP
 #define KEPLER_SOLVE_HPP
 
+#include <cmath>
 #include <cstddef>
 
+#include "./constants.hpp"
+#include "./reduction.hpp"
 #include "./refiners.hpp"
 #include "./starters.hpp"
 
@@ -14,8 +17,12 @@ inline void solve(const T& eccentricity, std::size_t size, const T* mean_anomaly
                   T* eccentric_anomaly, const Refiner& refiner = Refiner()) {
   const Starter starter(eccentricity);
   for (std::size_t i = 0; i < size; ++i) {
-    auto initial = starter.start(mean_anomaly[i]);
-    eccentric_anomaly[i] = refiner.refine(eccentricity, mean_anomaly[i], initial);
+    auto mean_anom_reduc = std::abs(mean_anomaly[i]);
+    bool high = range_reduce(mean_anom_reduc, mean_anom_reduc);
+    auto ecc_anom_reduc = starter.start(mean_anom_reduc);
+    ecc_anom_reduc = refiner.refine(eccentricity, mean_anom_reduc, ecc_anom_reduc);
+    eccentric_anomaly[i] = std::copysign(
+        high ? constants::twopi<T>() - ecc_anom_reduc : ecc_anom_reduc, mean_anomaly[i]);
   }
 }
 
--- test/test_refiners.cpp
+++ test/test_refiners.cpp
@@ -18,21 +18,22 @@ TEMPLATE_TEST_CASE("Iterative refiners", "[refiners]", (kepler::refiners::iterat
   using T = double;
   const T abs_tol = 5e-12;
   const size_t ecc_size = 10;
-  const size_t anom_size = 100;
+  const size_t anom_size = 1000;
   const TestType refiner;
   std::vector<T> ecc_anom_expect(anom_size), mean_anomaly(anom_size), ecc_anom_calc(anom_size);
 
   for (size_t n = 0; n < ecc_size; ++n) {
     const T eccentricity = n / T(ecc_size);
     for (size_t m = 0; m < anom_size; ++m) {
-      ecc_anom_expect[m] = kepler::constants::pi<T>() * m / T(anom_size - 1);
+      ecc_anom_expect[m] = 100. * m / T(anom_size - 1) - 50.;
       mean_anomaly[m] = ecc_anom_expect[m] - eccentricity * std::sin(ecc_anom_expect[m]);
     }
 
     kepler::solve(eccentricity, anom_size, mean_anomaly.data(), ecc_anom_calc.data(), refiner);
 
     for (size_t m = 0; m < anom_size; ++m) {
-      REQUIRE_THAT(ecc_anom_calc[m], WithinAbs(ecc_anom_expect[m], abs_tol));
+      REQUIRE_THAT(std::sin(ecc_anom_calc[m]), WithinAbs(std::sin(ecc_anom_expect[m]), abs_tol));
+      REQUIRE_THAT(std::cos(ecc_anom_calc[m]), WithinAbs(std::cos(ecc_anom_expect[m]), abs_tol));
     }
   }
 }
@@ -41,13 +42,13 @@ TEST_CASE("Nijenhuis / Markley", "[refiners]") {
   using T = double;
   const T abs_tol = 5e-12;
   const size_t ecc_size = 10;
-  const size_t anom_size = 100;
+  const size_t anom_size = 1000;
   std::vector<T> ecc_anom_expect(anom_size), mean_anomaly(anom_size), ecc_anom_calc(anom_size);
 
   for (size_t n = 0; n < ecc_size; ++n) {
     const T eccentricity = n / T(ecc_size);
     for (size_t m = 0; m < anom_size; ++m) {
-      ecc_anom_expect[m] = kepler::constants::pi<T>() * m / T(anom_size - 1);
+      ecc_anom_expect[m] = 100. * m / T(anom_size - 1) - 50.;
       mean_anomaly[m] = ecc_anom_expect[m] - eccentricity * std::sin(ecc_anom_expect[m]);
     }
 
@@ -55,7 +56,8 @@ TEST_CASE("Nijenhuis / Markley", "[refiners]") {
                                   ecc_anom_calc.data());
 
     for (size_t m = 0; m < anom_size; ++m) {
-      REQUIRE_THAT(ecc_anom_calc[m], WithinAbs(ecc_anom_expect[m], abs_tol));
+      REQUIRE_THAT(std::sin(ecc_anom_calc[m]), WithinAbs(std::sin(ecc_anom_expect[m]), abs_tol));
+      REQUIRE_THAT(std::cos(ecc_anom_calc[m]), WithinAbs(std::cos(ecc_anom_expect[m]), abs_tol));
     }
   }
 }
