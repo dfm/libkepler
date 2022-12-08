1d2beb5728141425936e964a8db850632406b07c
Dan F-M <foreman.mackey@gmail.com>
2022-12-08 12:17:12 -0500

    different sincos

--- include/kepler/math.hpp
+++ include/kepler/math.hpp
@@ -110,23 +110,69 @@ inline xs::batch<double, A> short_sin_eval(const xs::batch<double, A>& x) {
                            0x3de6124613a86d09, 0x3d6ae7f3e733b81f>(-x2);
 }
 
+template <typename T>
+static inline T cos_eval(const T&);
+template <typename T>
+static inline T sin_eval(const T&, const T&);
+
+template <>
+inline float cos_eval<float>(const float& z) {
+  float y = horner_static<float, 0x3d2aaaa5, 0xbab60619, 0x37ccf5ce>(z);
+  return 1. + fma(z, -0.5f, y * z * z);
+}
+
+template <>
+inline float sin_eval<float>(const float& z, const float& x) {
+  float y = horner_static<float, 0xbe2aaaa2, 0x3c08839d, 0xb94ca1f9>(z);
+  return fma(y * z, x, x);
+}
+
+template <>
+inline double cos_eval<double>(const double& z) {
+  double y = horner_static<double, 0x3fe0000000000000ull, 0xbfa5555555555551ull,
+                           0x3f56c16c16c15d47ull, 0xbefa01a019ddbcd9ull, 0x3e927e4f8e06d9a5ull,
+                           0xbe21eea7c1e514d4ull, 0x3da8ff831ad9b219ull>(z);
+  return 1. - y * z;
+}
+
+template <>
+inline double sin_eval<double>(const double& z, const double& x) {
+  double y =
+      horner_static<double, 0xbfc5555555555548ull, 0x3f8111111110f7d0ull, 0xbf2a01a019bfdf03ull,
+                    0x3ec71de3567d4896ull, 0xbe5ae5e5a9291691ull, 0x3de5d8fd1fcf0ec1ull>(z);
+  return fma(y * z, x, x);
+}
+
+template <typename T>
+inline unsigned short_reduce(const T& x, T& xr) {
+  if (x < constants::pio4<T>()) {
+    xr = x;
+    return 0;
+  } else if (x < constants::threepio4<T>()) {
+    xr = x - constants::pio2<T>();
+    return 1;
+  }
+  xr = x - constants::pi<T>();
+  return 2;
+}
+
 }  // namespace detail
 
-// NOTE: only valid for x in [0, pi]
+// NOTE: only valid for x in [0, pi], with a tiny bit of leeway...
 template <typename T>
 inline std::pair<T, T> sincos(const T& x) {
-  T s, c;
-  if (x < constants::pio4<T>()) {
-    s = detail::short_sin_eval(x);
-    c = std::sqrt(1 - s * s);
-  } else if (x > constants::threepio4<T>()) {
-    s = detail::short_sin_eval(constants::pi<T>() - x);
-    c = -std::sqrt(1 - s * s);
-  } else {
-    c = detail::short_sin_eval(constants::pio2<T>() - x);
-    s = std::sqrt(1 - c * c);
+  const auto sgn = std::signbit(x) ? T(-1.) : T(1.);
+  auto xr = std::abs(x);
+  const auto n = detail::short_reduce(xr, xr);
+  const auto z = xr * xr;
+  const auto se = detail::sin_eval(z, xr);
+  const auto ce = detail::cos_eval(z);
+  if (n == 0) {
+    return std::make_pair(sgn * se, ce);
+  } else if (n == 1) {
+    return std::make_pair(sgn * ce, -se);
   }
-  return std::make_pair(s, c);
+  return std::make_pair(sgn * -se, -ce);
 }
 
 template <typename A, typename T>
